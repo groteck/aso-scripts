37dc0493c72ec58f95461cbc92e4090b004509cb
groteck <groteck@gmail.com>
2013-01-16 03:55:25 +0000

    added gitignored and now exam 20111123 it's ok

--- examen-20111123-primera_parte.bash
+++ examen-20111123-primera_parte.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-users=( `cat "/etc/passwd" | awk 'BEGIN {FS=":"} {$3 > 499} {print $1}'`)
+users=( `cat "/etc/passwd" | awk 'BEGIN {FS=":"; OFS=" "} {$3 > 499} {print $1}'`)
+uids=( `cat "/etc/passwd" | awk 'BEGIN {FS=":"; OFS=" "} {$3 > 499} {print $3}'`)
+names=( `cat "/etc/passwd" | awk 'BEGIN {FS=":"; OFS=" "} {$3 > 499} {print $5}'`)
 
-for user in "${users[@]}"
+for index in "${!users[@]}"
 do
-  full= $user
-  groups=( `grep $user "/etc/group" | awk 'BEGIN {FS=":"} {print $1}'`)
+  full="${users[$index]} ${names[$index]} (${uids[$index]}):"
+  groups=( `grep ${users[$index]} "/etc/group" | awk 'BEGIN {FS=":"} {print $1}'`)
   for group in "${groups[@]}"
   do
     full="$full $group"
   done
-  print "$full"
+  echo "$full"
 done
